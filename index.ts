3bcc3fe1a8817f1fb8ebd2f2216345dc4d9d3ea8
akshitdandyan <akshitdandyan5287@gmail.com>
2022-12-19 13:54:32 +0530

    fixes in video trim & start frontend

--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
-import express,{Request,Response} from 'express';
-import './controllers/telegramControllers';
+import express, { Request, Response } from "express";
+import { resolve } from "path";
+// import "./controllers/telegramControllers";
 
 const app = express();
 
-app.get("/",(_req:Request, res:Response)=>{
-    res.status(200).send("COOL").end();
+const __dirname = resolve();
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (_req: Request, res: Response) => {
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 const PORT = 3000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("Media converter running on port", PORT);
 });
