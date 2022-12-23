f82db1caf0633014e604760a166cbce09a9dcd56
akshitdandyan <akshitdandyan5287@gmail.com>
2022-12-23 23:32:13 +0530

    @bug-fixes/commands-to-choose-feature

--- controllers/telegramControllers.ts
+++ controllers/telegramControllers.ts
@@ -2,218 +2,223 @@ import { createWriteStream, createReadStream } from "fs";
 import { randomUUID } from "crypto";
 import teleBot from "../@config/telegram";
 import {
-  getMimeType,
-  handleFeatureReply,
-  isFeature,
-  isOption,
-  newMediaStoragePath,
-  removeFile,
-  videoExceedsSizeLimit,
+    getMimeType,
+    handleFeatureReply,
+    isFeature,
+    isOption,
+    newMediaStoragePath,
+    removeFile,
+    videoExceedsSizeLimit,
 } from "../helpers/helpers";
 import {
-  extractAudio,
-  modifySpeed,
-  reduceSize,
-  removeAudio,
-  trimVideo,
+    extractAudio,
+    modifySpeed,
+    reduceSize,
+    removeAudio,
+    trimVideo,
 } from "../ffmpeg/ffmpeg";
 import { VIDEO_FEATURES } from "../@config/constants";
 import clients from "./clients";
 
 teleBot.on("message", (message) => {
-  console.log("[telegramControllers.js] Recieved message:", message.text);
-  if (!message.text) {
-    return;
-  }
-  if (message.text === "/start") {
-    teleBot.setMyCommands([
-      { command: "/start", description: "Start with us" },
-      {
-        command: "/" + VIDEO_FEATURES.reduceSize,
-        description: "Reduce video file size with ulta compression",
-      },
-      {
-        command: "/" + VIDEO_FEATURES.trimVideo,
-        description: "Trim video within a time range",
-      },
-      {
-        command: "/" + VIDEO_FEATURES.removeAudio,
-        description: "Remove audio from video",
-      },
-      {
-        command: "/" + VIDEO_FEATURES.extractAudio,
-        description: "Get music from video",
-      },
-      {
-        command: "/" + VIDEO_FEATURES.modifySpeed,
-        description: "Modify payback speed of video",
-      },
-    ]);
-    teleBot.sendMessage(
-      message.chat.id,
-      `Hello ${message.chat.first_name}🌟\nI can perform following tasks as of now. Please choose one.`,
-      {
-        reply_to_message_id: message.message_id,
-        reply_markup: {
-          force_reply: true,
-          resize_keyboard: true,
-          remove_keyboard: true,
-          one_time_keyboard: true,
-          keyboard: [
-            [
-              {
-                text: VIDEO_FEATURES.trimVideo,
-              },
-              {
-                text: VIDEO_FEATURES.reduceSize,
-              },
-            ],
-            [
-              {
-                text: VIDEO_FEATURES.removeAudio,
-              },
-              {
-                text: VIDEO_FEATURES.extractAudio,
-              },
-            ],
-            [
-              {
-                text: VIDEO_FEATURES.modifySpeed,
-              },
-            ],
-          ],
-        },
-      }
-    );
-  } else if (isFeature(message.text)) {
-    console.log("[telegramControllers.js] client sent feature details");
-    const { replyMessage, featureType } = handleFeatureReply(message.text);
-
-    clients.add({
-      firstName: message.chat.first_name,
-      id: randomUUID(),
-      feature: featureType,
-      status: "joined",
-      teleId: message.chat.id,
-      username: message.chat.username,
-    });
-
-    teleBot.sendMessage(message.chat.id, replyMessage);
-  } else {
-    console.log("[telegramControllers.js] client may have sent options");
-    const optionRes = isOption(message.text);
-    teleBot.sendMessage(message.chat.id, optionRes.message);
-    if (optionRes.success) {
-      clients.attachOptions(message.chat.username, optionRes.options);
+    console.log("[telegramControllers.js] Recieved message:", message.text);
+    if (!message.text) {
+        return;
     }
-  }
+    if (message.text === "/start") {
+        teleBot.setMyCommands([
+            { command: "/start", description: "Start with us" },
+            {
+                command: "/reducevideosize",
+                description: "Reduce video file size with ulta compression",
+            },
+            {
+                command: "/trimvideo",
+                description: "Trim video within a time range",
+            },
+            {
+                command: "/removeaudio",
+                description: "Remove audio from video",
+            },
+            {
+                command: "/extractmusic",
+                description: "Get music from video",
+            },
+            {
+                command: "/modifyspeed",
+                description: "Modify payback speed of video",
+            },
+        ]);
+        teleBot.sendMessage(
+            message.chat.id,
+            `Hello ${message.chat.first_name}🌟\nI can perform following tasks as of now. Please choose one.`,
+            {
+                reply_to_message_id: message.message_id,
+                reply_markup: {
+                    force_reply: true,
+                    resize_keyboard: true,
+                    remove_keyboard: true,
+                    one_time_keyboard: true,
+                    keyboard: [
+                        [
+                            {
+                                text: VIDEO_FEATURES.trimVideo,
+                            },
+                            {
+                                text: VIDEO_FEATURES.reduceSize,
+                            },
+                        ],
+                        [
+                            {
+                                text: VIDEO_FEATURES.removeAudio,
+                            },
+                            {
+                                text: VIDEO_FEATURES.extractAudio,
+                            },
+                        ],
+                        [
+                            {
+                                text: VIDEO_FEATURES.modifySpeed,
+                            },
+                        ],
+                    ],
+                },
+            }
+        );
+    } else if (isFeature(message.text) || message.text.startsWith("/")) {
+        console.log("[telegramControllers.js] client sent feature details");
+        const { replyMessage, featureType } = handleFeatureReply(message.text);
+
+        clients.add({
+            firstName: message.chat.first_name,
+            id: randomUUID(),
+            feature: featureType,
+            status: "joined",
+            teleId: message.chat.id,
+            username: message.chat.username,
+        });
+
+        teleBot.sendMessage(message.chat.id, replyMessage);
+    } else {
+        console.log("[telegramControllers.js] client may have sent options");
+        const optionRes = isOption(message.text);
+        teleBot.sendMessage(message.chat.id, optionRes.message);
+        if (optionRes.success) {
+            clients.attachOptions(message.chat.username, optionRes.options);
+        }
+    }
 });
 
 teleBot.on("video", async (res) => {
-  console.log("[telegramControllers.js] video", res);
+    console.log("[telegramControllers.js] video", res);
 
-  if (res.video && res.video.file_id) {
-    if (videoExceedsSizeLimit(res.video.file_size)) {
-      teleBot.sendMessage(
-        res.chat.id,
-        "We are at very early stage, cannot afford to process files larger than 15 MB in size as of now.😶‍🌫️"
-      );
-      return;
+    if (res.video && res.video.file_id) {
+        if (videoExceedsSizeLimit(res.video.file_size)) {
+            teleBot.sendMessage(
+                res.chat.id,
+                "We are at very early stage, cannot afford to process files larger than 15 MB in size as of now.😶‍🌫️"
+            );
+            return;
+        }
+        const client = clients.get(res.chat.username);
+
+        if (!client) {
+            console.log("[telegramControllers.js] client not found");
+            // TODO: add logic: if user directly sends media, send feature options again
+            return;
+        }
+
+        const fileStream = teleBot.getFileStream(res.video.file_id);
+
+        const fileName = res.video["file_name"]
+            ? res.video["file_name"]
+            : "untitled-69-"
+                  .concat(new Date().getTime().toString())
+                  .concat(".mp4");
+
+        const mediaStoragePath = newMediaStoragePath(fileName);
+
+        clients.attachMedia(res.chat.username, {
+            duration: res.video.duration,
+            fileId: res.video.file_id,
+            fileUniqueId: res.video.file_unique_id,
+            fileName: fileName,
+            inputLocation: mediaStoragePath,
+            mime: getMimeType(fileName),
+            size: res.video.file_size,
+            type: "video",
+        });
+
+        fileStream.on("end", async () => {
+            console.log("File saved");
+            let outputPath = "";
+            let caption = "";
+            if (client.feature === "video-reduce-size") {
+                outputPath = await reduceSize(
+                    mediaStoragePath,
+                    fileName,
+                    res.video.file_size
+                );
+                caption = "✅ Video size reduced";
+            } else if (
+                client.feature === "video-trim" &&
+                client.options.length === 2
+            ) {
+                outputPath = await trimVideo({
+                    fileLocation: mediaStoragePath,
+                    fileName: fileName,
+                    startTime: Number(client.options[0]),
+                    endTime: Number(client.options[1]),
+                });
+                caption = "✅ Video Trimmed";
+            } else if (client.feature === "remove-audio") {
+                outputPath = await removeAudio({
+                    fileLocation: mediaStoragePath,
+                    fileName: fileName,
+                });
+                caption = "✅ Audio Removed";
+            } else if (client.feature === "extract-music") {
+                outputPath = await extractAudio({
+                    fileLocation: mediaStoragePath,
+                    fileName: fileName,
+                });
+                caption = "✅ Music extracted";
+            } else if (client.feature === "playback-speed") {
+                outputPath = await modifySpeed({
+                    fileLocation: mediaStoragePath,
+                    fileName: fileName,
+                    pts: "0.25",
+                });
+                caption = "✅ Playback speed modified";
+            } else {
+                // TODO: handle if feature not chosen
+                return;
+            }
+
+            clients.attachOutputLocation(res.chat.username, outputPath);
+
+            console.log("⬆️Sending output to client...");
+            const outputStream = createReadStream(outputPath);
+            if (outputPath.includes("mp3")) {
+                await teleBot.sendAudio(res.chat.id, outputStream, {
+                    caption,
+                });
+            } else {
+                await teleBot.sendVideo(res.chat.id, outputStream, {
+                    caption,
+                });
+            }
+
+            console.log("✅ Processed file sent to client");
+            clients.remove(client.username);
+            removeFile(client.media.inputLocation);
+            removeFile(client.media.outputLocation);
+        });
+
+        fileStream.on("error", () => {
+            console.log("Error in file saving");
+        });
+
+        fileStream.pipe(createWriteStream(mediaStoragePath));
     }
-    const client = clients.get(res.chat.username);
-
-    if (!client) {
-      console.log("[telegramControllers.js] client not found");
-      // TODO: add logic: if user directly sends media, send feature options again
-      return;
-    }
-
-    const fileStream = teleBot.getFileStream(res.video.file_id);
-
-    const fileName = res.video["file_name"]
-      ? res.video["file_name"]
-      : "untitled-69-".concat(new Date().getTime().toString()).concat(".mp4");
-
-    const mediaStoragePath = newMediaStoragePath(fileName);
-
-    clients.attachMedia(res.chat.username, {
-      duration: res.video.duration,
-      fileId: res.video.file_id,
-      fileUniqueId: res.video.file_unique_id,
-      fileName: fileName,
-      inputLocation: mediaStoragePath,
-      mime: getMimeType(fileName),
-      size: res.video.file_size,
-      type: "video",
-    });
-
-    fileStream.on("end", async () => {
-      console.log("File saved");
-      let outputPath = "";
-      let caption = "";
-      if (client.feature === "video-reduce-size") {
-        outputPath = await reduceSize(
-          mediaStoragePath,
-          fileName,
-          res.video.file_size
-        );
-        caption = "✅ Video size reduced";
-      } else if (client.feature === "video-trim" && client.options.length===2) {
-        outputPath = await trimVideo({
-          fileLocation: mediaStoragePath,
-          fileName: fileName,
-          startTime: Number(client.options[0]),
-          endTime: Number(client.options[1]),
-        });
-        caption = "✅ Video Trimmed";
-      } else if (client.feature === "remove-audio") {
-        outputPath = await removeAudio({
-          fileLocation: mediaStoragePath,
-          fileName: fileName,
-        });
-        caption = "✅ Audio Removed";
-      } else if (client.feature === "extract-music") {
-        outputPath = await extractAudio({
-          fileLocation: mediaStoragePath,
-          fileName: fileName,
-        });
-        caption = "✅ Music extracted";
-      } else if (client.feature === "playback-speed") {
-        outputPath = await modifySpeed({
-          fileLocation: mediaStoragePath,
-          fileName: fileName,
-          pts: "0.25",
-        });
-        caption = "✅ Playback speed modified";
-      } else {
-        // TODO: handle if feature not chosen
-        return;
-      }
-
-      clients.attachOutputLocation(res.chat.username, outputPath);
-
-      console.log("⬆️Sending output to client...");
-      const outputStream = createReadStream(outputPath);
-      if (outputPath.includes("mp3")) {
-        await teleBot.sendAudio(res.chat.id, outputStream, {
-          caption,
-        });
-      } else {
-        await teleBot.sendVideo(res.chat.id, outputStream, {
-          caption,
-        });
-      }
-
-      console.log("✅ Processed file sent to client");
-      clients.remove(client.username);
-      removeFile(client.media.inputLocation);
-      removeFile(client.media.outputLocation);
-    });
-
-    fileStream.on("error", () => {
-      console.log("Error in file saving");
-    });
-
-    fileStream.pipe(createWriteStream(mediaStoragePath));
-  }
 });
--- helpers/helpers.ts
+++ helpers/helpers.ts
@@ -4,149 +4,156 @@ import { videoMimeTypes, VIDEO_FEATURES } from "../@config/constants";
 import { Feature } from "../controllers/clients";
 
 export const mediaExtension = (filename: string) => {
-  return filename.match(/\.[0-9a-z]+$/i)[0];
+    return filename.match(/\.[0-9a-z]+$/i)[0];
 };
 
 export const newMediaStoragePath = (filename: string) => {
-  if (!existsSync("./client-media")) {
-    mkdirSync("./client-media");
-  }
-  return `./client-media/${filename}@uuid:${randomUUID()}${mediaExtension(
-    filename
-  )}`;
+    if (!existsSync("./client-media")) {
+        mkdirSync("./client-media");
+    }
+    return `./client-media/${filename}@uuid:${randomUUID()}${mediaExtension(
+        filename
+    )}`;
 };
 
 export const mediaOutputPath = (filename: string, isAudio = false) => {
-  if (!existsSync("./server-media")) {
-      mkdirSync("./server-media");
-  }
-  if (!isAudio) {
-      return `./server-media/${filename}`;
-  }
-  return `./server-media/${filename.replace(".mp4", ".mp3")}`;
+    if (!existsSync("./server-media")) {
+        mkdirSync("./server-media");
+    }
+    if (!isAudio) {
+        return `./server-media/${filename}`;
+    }
+    return `./server-media/${filename.replace(".mp4", ".mp3")}`;
 };
 
-
 export const getBitrate = (bytes: number) => {
-  const diff = Math.floor(bytes / 1000000);
-  if (diff < 5) {
-    return 128;
-  }
-  return Math.floor(diff * 28 * 1.1);
+    const diff = Math.floor(bytes / 1000000);
+    if (diff < 5) {
+        return 128;
+    }
+    return Math.floor(diff * 28 * 1.1);
 };
 
 export const formatSecondsIntoHHMMSS = (seconds: number) => {
-  const date = new Date(seconds * 1000);
-  const hh = date.getUTCHours();
-  const mm = date.getUTCMinutes();
-  const ss = date.getSeconds();
-  return `${hh}:${mm}:${ss}`;
+    const date = new Date(seconds * 1000);
+    const hh = date.getUTCHours();
+    const mm = date.getUTCMinutes();
+    const ss = date.getSeconds();
+    return `${hh}:${mm}:${ss}`;
 };
 
-export const getMimeType = (filename:string) => {
-    const fileExtension = filename.split('.').pop();
+export const getMimeType = (filename: string) => {
+    const fileExtension = filename.split(".").pop();
     const mimeType = videoMimeTypes[fileExtension];
-    return mimeType || 'application/octet-stream';
-    };
+    return mimeType || "application/octet-stream";
+};
 
 export function handleFeatureReply(command: string) {
-  let replyMessage = "";
-  let needReply = false;
-  let featureType: Feature = "not-choosen";
+    let replyMessage = "";
+    let needReply = false;
+    let featureType: Feature = "not-choosen";
+    console.log("[helpers.js] command", command);
 
-  switch (command) {
-    case VIDEO_FEATURES.reduceSize:
-      replyMessage = "Okay, send the video whose size is to be reduced 🎦";
-      featureType = "video-reduce-size";
-      break;
-    case VIDEO_FEATURES.trimVideo:
-      replyMessage =
-        "Okay, please type the start and end duration in seconds.\nFor example, if you want to trim a video from 2s to 6s, type 2 6";
-      needReply = true;
-      featureType = "video-trim";
-      break;
-    case VIDEO_FEATURES.removeAudio:
-      replyMessage = "Okay, send the video which needs to be muted 🔇";
-      featureType = "remove-audio";
-      break;
-    case VIDEO_FEATURES.extractAudio:
-      replyMessage = "Okay, send the video from which you want to extract music 🎵";
-      featureType = "extract-music";
-      break;
-    case VIDEO_FEATURES.modifySpeed:
-      replyMessage = "Okay, send the video whose playback speed you want to modify ⌛.";
-      featureType = "playback-speed";
-      needReply = true;
-      break;
-    default:
-      replyMessage = "Woops! Please choose only from given options.";
-      break;
-  }
+    switch (command) {
+        case VIDEO_FEATURES.reduceSize:
+        case "/reducevideosize":
+            replyMessage = "Okay, send the video which needs to be 🤏🏼";
+            featureType = "video-reduce-size";
+            break;
+        case VIDEO_FEATURES.trimVideo:
+        case "/trimvideo":
+            replyMessage =
+                "Okay, please type the start and end duration in seconds.\nFor example, if you want to trim a video from 2s to 6s, type 2 6";
+            needReply = true;
+            featureType = "video-trim";
+            break;
+        case VIDEO_FEATURES.removeAudio:
+        case "/removeaudio":
+            replyMessage = "Okay, send the video which needs to be muted 🔇";
+            featureType = "remove-audio";
+            break;
+        case VIDEO_FEATURES.extractAudio:
+        case "/extractaudio":
+            replyMessage =
+                "Okay, send the video from which you want to extract music 🎵";
+            featureType = "extract-music";
+            break;
+        case VIDEO_FEATURES.modifySpeed:
+        case "/modifyspeed":
+            replyMessage =
+                "Okay, send the video whose playback speed you want to modify ⌛.";
+            featureType = "playback-speed";
+            needReply = true;
+            break;
+        default:
+            replyMessage = "Woops! Please choose only from given options.";
+            break;
+    }
 
-  return {
-    needReply,
-    replyMessage,
-    featureType,
-  };
+    return {
+        needReply,
+        replyMessage,
+        featureType,
+    };
 }
 
 export function isFeature(reply: string) {
-  for (var prop in VIDEO_FEATURES) {
-    if (VIDEO_FEATURES[prop] === reply) {
-      return true;
+    for (var prop in VIDEO_FEATURES) {
+        if (VIDEO_FEATURES[prop] === reply) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 export function isOption(reply: string) {
-  try {
-    // 1. durations for video trim feature
-    const splitted = reply.split(" ");
-    if (splitted.length < 2) {
-      return {
-        success: false,
-        message:
-          "🙄Umm, that's not enough to proceed with. Please write both start and end duration in following format: \n\n 5 10",
-      };
+    try {
+        // 1. durations for video trim feature
+        const splitted = reply.split(" ");
+        if (splitted.length < 2) {
+            return {
+                success: false,
+                message:
+                    "🙄Umm, that's not enough to proceed with. Please write both start and end duration in following format: \n\n 5 10",
+            };
+        }
+        const startTime = Number(splitted[0]);
+        const endTime = Number(splitted[splitted.length - 1]);
+        if (startTime >= endTime) {
+            return {
+                success: false,
+                message:
+                    "Sorry, we cannot produce videos in duration with minus seconds😟",
+            };
+        }
+        return {
+            success: true,
+            message: "Amazing! Send the video now🤩",
+            options: [startTime.toString(), endTime.toString()],
+        };
+    } catch (error) {
+        console.log("[helpers.ts] isOption error", error);
+        return {
+            success: false,
+            message:
+                "Agh!! Something went wrong😟 Please take a snapshot and email it to us so that we can fix it.",
+        };
     }
-    const startTime = Number(splitted[0]);
-    const endTime = Number(splitted[splitted.length - 1]);
-    if (startTime >= endTime) {
-      return {
-        success: false,
-        message:
-          "Sorry, we cannot produce videos in duration with minus seconds😟",
-      };
-    }
-    return {
-      success: true,
-      message: "Amazing! Send the video now🤩",
-      options:[startTime.toString(),endTime.toString()]
-    };
-  } catch (error) {
-    console.log("[helpers.ts] isOption error", error)
-    return {
-      success: false,
-      message:
-        "Agh!! Something went wrong😟 Please take a snapshot and email it to us so that we can fix it.",
-    };
-  }
 }
 
 export const videoExceedsSizeLimit = (size: number) => {
     return size > 15 * 1024 * 1024;
-}
+};
 
-export const removeFile = (path:string) => {
+export const removeFile = (path: string) => {
     try {
-        if(statSync(path)){
+        if (statSync(path)) {
             unlinkSync(path);
             console.log("🧹File removed");
             return;
         }
         console.log("🧹File not found to remove");
     } catch (error) {
-        console.log("[helpers.ts] removeFile error:", error)
+        console.log("[helpers.ts] removeFile error:", error);
     }
-}
+};
